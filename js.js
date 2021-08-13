1bd31bb5a8c756567db41457fb4a2f130d61dd79
joas24 <88119386+joas24@users.noreply.github.com>
2021-08-13 21:59:55 +0900

    Add files via upload

--- js.js
+++ js.js
@@ -1,13 +1,17 @@
 var col = {
     body: function (color, color2) {
         $('body').css('backgroundColor', color);
-        $('body').css('color', color2);
-        // document.querySelector('body').style.backgroundColor = color;
-        // document.querySelector('body').style.color = color2;            
+        $('body').css('color', color2);      
     },
+
     a: function (color) {
         var aa = document.querySelectorAll('a');
 
         for (var i = 0; i < aa.length; i++) aa[i].style.color = color;
+    },
+    
+    bt: function(color, color2){
+        $('#bt').css('backgroundColor', color);
+        $('#bt').css('color', color2);
     }
 }
